791f193e70ba6bbf34d04de6459f188538c9e638
erkan.obut <erkan.obut@teamdefinex.com>
2019-09-03 13:47:55 +0300

    test commit

--- src/main/java/Home/HomeController.java
+++ src/main/java/Home/HomeController.java
@@ -1,4 +1,14 @@
 package Home;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
 }
